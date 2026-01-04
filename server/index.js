1a953739767bd048472ab75cdc18be6075691653
huanguang <380608682@qq.com>
2026-01-04 10:57:22 +0800

    1

--- server/index.js
+++ server/index.js
@@ -129,6 +129,10 @@ app.delete('/dishes/:id', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
